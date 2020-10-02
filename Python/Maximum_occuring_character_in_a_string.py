50f07309bd8d7896fbdd92e68d928d66a152d863
ShilpaRajeev <58106715+ShilpaRajeev@users.noreply.github.com>
2020-10-02 21:35:06 +0530

    Create Maximum_occuring_character_in_a_string.py

---
+++ Python/Maximum_occuring_character_in_a_string.py
@@ -0,0 +1,12 @@
+str=input("Please enter a String")
+d={}
+c={}
+for i in str:
+    if i in c:
+        c[i]+= 1
+    else:
+        c[i]=1
+    d.update({i:c[i]})
+occ=max(d.values())
+str= max(d,key = d.get)
+print(s,occ)
